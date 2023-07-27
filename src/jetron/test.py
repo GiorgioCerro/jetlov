19cb247c4ecbe536f87a731dfaf7d45595556a27
G.Cerro <giorgiocerro96@gmail.com>
2023-07-27 10:48:38 +0100

    cleaned and added data to shared path

--- src/jetron/test.py
+++ src/jetron/test.py
@@ -1,7 +1,7 @@
 from lundnet.dgl_dataset import DGLGraphDatasetLund as Dataset
 from pathlib import Path
 
-path = "/scratch/gc2c20/data/test/"
+path = "/home/gc2c20/office_share/giorgio/w_tagging/"
 dataset = Dataset(filepath_bkg=Path(path+"test_QCD_500GeV.json.gz"),
                 filepath_sig=Path(path+"test_WW_500GeV.json.gz"),
                 nev=-1, n_samples=100)
